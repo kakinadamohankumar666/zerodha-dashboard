a5f9bbf6e93cdb53309c560b65ffb189de9cf4c5
mohan kumar <varshit666@gmail.com>
2025-07-12 16:04:03 +0530

    Fix CORS to allow frontend Vercel domain

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,13 @@ const url = process.env.MONGO_URL;
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:["http://localhost:3000"],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials: true
-}))
+  origin: [
+  "https://zerodha-dashboard-6ajw-4d7147r6v-kakinada-mohankumars-projects.vercel.app"],
+  credentials: true
+}));
 
 app.use(bodyParser.json());
 
