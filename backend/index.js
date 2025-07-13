70cdf47adfbeeb55189084dc29d2c2cce55fc257
mohan kumar <144615231+kakinadamohankumar666@users.noreply.github.com>
2025-07-13 09:54:46 +0530

    Update frontend url

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: [ "http://localhost:3000"],
+  origin: [ "https://zerodha-dashboard-sigma.vercel.app/"],
   credentials: true
 }));
 
