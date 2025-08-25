8d429166dc19edc9fb4b34f3dcc7bd75a42e129c
mohan kumar <varshit666@gmail.com>
2025-08-25 08:31:49 +0530

    cookie fix

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -11,9 +11,14 @@ module.exports.Signup = async (req, res, next) => {
     }
     const user = await User.create({ email, password, username, createdAt });
     const token = createSecretToken(user._id);
+    // res.cookie("token", token, {
+    //   withCredentials: true,
+    //   httpOnly: false,
+    // });
     res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false,
+      httpOnly: true,
+      secure: true, // required in production (HTTPS)
+      sameSite: "None", // allow cross-site cookies
     });
     res
       .status(201)
@@ -27,25 +32,32 @@ module.exports.Signup = async (req, res, next) => {
 module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    if(!email || !password ){
-      return res.json({message:'All fields are required'})
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
     }
     const user = await User.findOne({ email });
-    if(!user){
-      return res.json({message:'Incorrect password or email' }) 
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
     }
-    const auth = await bcrypt.compare(password,user.password)
+    const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return res.json({message:'Incorrect password or email' }) 
+      return res.json({ message: "Incorrect password or email" });
     }
-     const token = createSecretToken(user._id);
-     res.cookie("token", token, {
-       withCredentials: true,
-       httpOnly: false,
-     });
-     res.status(201).json({ message: "User logged in successfully", success: true });
-     next()
+    const token = createSecretToken(user._id);
+    //  res.cookie("token", token, {
+    //    withCredentials: true,
+    //    httpOnly: false,
+    //  });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // required in production (HTTPS)
+      sameSite: "None", // allow cross-site cookies
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
   } catch (error) {
     console.error(error);
   }
-}
+};
